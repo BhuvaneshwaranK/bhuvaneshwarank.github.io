f64211a3672e01c20ed5854887d5cf1b94585c74
Bhuvaneshwaran <cselakshmanan@gmail.com>
2017-09-07 15:44:02 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,14 +20,14 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "http://formspree.io/cselakshmanan@gmail.com",
         type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
+        // data: {
+        //   name: name,
+        //   phone: phone,
+        //   email: email,
+        //   message: message
+        // },
         cache: false,
         success: function() {
           // Success message
