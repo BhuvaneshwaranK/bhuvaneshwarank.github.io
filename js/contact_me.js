4a1d485f8ea09a21ccdefcc0cb0d3258cc59e75c
Bhuvaneshwaran <cselakshmanan@gmail.com>
2017-09-07 20:05:21 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,27 +1,12 @@
 
 $( document ).ready(function() {
-//     $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-//   $("#quote_text").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-// });
     $.ajax({
-        url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+        url: "http://quotes.stormconsultancy.co.uk/random.json",
         method:"GET",
-        contentType: "application/json;odata=verbose",
-        headers: {
-          "X-Mashape-Key": "1dxu1jK3JymshsynO0mMMWuQOYHxp1pR5Q8jsnJGSzuBNxtBp1",
-           "Content-Type": "application/x-www-form-urlencoded",
-           "Accept": "application/json"
-        },
-        xhrFields: {
-            withCredentials: false
-        },
+        dataType: 'json',
         success: function (data) { 
-            console.log("data",data);
-//             data=(JSON.parse(data)); 
             var quote = data.quote;
             var author = data.author;
-//             quote = "A year spent in artificial intelligence is enough to make one believe in God.";
-//             author = "Alan Perlis";
             $('#quote_text').append("<p>"+ quote + "</p><p>&mdash; " + author + "</p>");
             console.log("success");
             
@@ -71,44 +56,7 @@ $(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
           }, 1000);
 
-      // $.ajax({
-      //   url: "http://formspree.io/cselakshmanan@gmail.com",
-      //   type: "POST",
-      //   data: {
-      //     name: name,
-      //     phone: phone,
-      //     email: email,
-      //     message: message
-      //   },
-      //   cache: false,
-      //   success: function() {
-      //     // Success message
-      //     $('#success').html("<div class='alert alert-success'>");
-      //     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-      //       .append("</button>");
-      //     $('#success > .alert-success')
-      //       .append("<strong>Your message has been sent. </strong>");
-      //     $('#success > .alert-success')
-      //       .append('</div>');
-      //     //clear all fields
-      //     $('#contactForm').trigger("reset");
-      //   },
-      //   error: function() {
-      //     // Fail message
-      //     $('#success').html("<div class='alert alert-danger'>");
-      //     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-      //       .append("</button>");
-      //     $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-      //     $('#success > .alert-danger').append('</div>');
-      //     //clear all fields
-      //     $('#contactForm').trigger("reset");
-      //   },
-      //   complete: function() {
-      //     setTimeout(function() {
-      //       $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-      //     }, 1000);
-      //   }
-      // });
+  
     },
     filter: function() {
       return $(this).is(":visible");
